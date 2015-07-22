637303c4c3986432f7ff33503e36f2b03af81f34
Div Shekhar <div@txtlabs.com>
2015-07-21 23:38:37 -0700

    AppendEntries receiver step 4, and error handling

--- consensus_append_entries.go
+++ consensus_append_entries.go
@@ -4,30 +4,36 @@
 
 package raft
 
-func (cm *ConsensusModule) _processRpc_AppendEntries(appendEntries AppendEntries) bool {
+func (cm *ConsensusModule) _processRpc_AppendEntries(appendEntries AppendEntries) (bool, error) {
 
 	leaderCurrentTerm := appendEntries.term
 	prevLogIndex := appendEntries.prevLogIndex
 
 	// 1. Reply false if term < currentTerm (#5.1)
 	if leaderCurrentTerm < cm.persistentState.currentTerm {
-		return false
+		return false, nil
 	}
 
 	// 2. Reply false if log doesn't contain an entry at prevLogIndex whose term
 	//      matches prevLogTerm (#5.3)
 	log := cm.persistentState.log
 	if log.getIndexOfLastEntry() < prevLogIndex {
-		return false
+		return false, nil
 	}
 
 	// 3. If an existing entry conflicts with a new one (same index
 	// but different terms), delete the existing entry and all that
 	// follow it (#5.3)
-	if log.getTermAtIndex(prevLogIndex) != leaderCurrentTerm {
+	if log.getTermAtIndex(prevLogIndex) != appendEntries.prevLogTerm {
 		log.deleteFromIndexToEnd(prevLogIndex)
-		return false
+		return false, nil
 	}
 
-	return true
+	// 4. Append any new entries not already in the log
+	err := log.appendEntriesAfterIndex(appendEntries.entries, prevLogIndex)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
