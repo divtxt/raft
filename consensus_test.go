316d7ecabe1491a98d09e53de852b451d2f81fdd
Div Shekhar <div@txtlabs.com>
2015-07-25 21:02:14 -0700

    Use NewConsensusModule to initialize

--- consensus_test.go
+++ consensus_test.go
@@ -4,19 +4,9 @@ import (
 	"testing"
 )
 
-func NewConsensusModule() *ConsensusModule {
-	return &ConsensusModule{
-		// #5.2-p1s2: When servers start up, they begin as followers
-		FOLLOWER,
-		PersistentState{TEST_CURRENT_TERM, nil},
-		VolatileState{},
-	}
-}
-
 // #5.2-p1s2: When servers start up, they begin as followers
 func TestCMStartsAsFollower(t *testing.T) {
-	var cm *ConsensusModule
-	cm = NewConsensusModule()
+	cm := NewConsensusModule(PersistentState{TEST_CURRENT_TERM, nil})
 
 	if cm == nil {
 		t.Fatal()
