93f9903147c768eddc989be993121d2cc405447d
Div Shekhar <div@txtlabs.com>
2015-07-28 23:46:45 -0700

    Election timeout and election start

--- consensus.go
+++ consensus.go
@@ -6,11 +6,17 @@ type ConsensusModule interface {
 
 	// Process the given RPC
 	ProcessRpc(AppendEntries) (AppendEntriesReply, error)
+
+	// Election timeout occurred
+	ElectionTimeout()
 }
 
 // Initialize a consensus module with the given persistent state
-func NewConsensusModule(persistentState PersistentState, log Log) ConsensusModule {
-	return newConsensusModuleImpl(persistentState, log)
+func NewConsensusModule(
+	persistentState PersistentState,
+	log Log,
+) ConsensusModule {
+	return newConsensusModuleImpl(persistentState, log, nil)
 }
 
 // Internal implementation structure
@@ -21,8 +27,18 @@ type consensusModuleImpl struct {
 	volatileState   VolatileState
 }
 
+// Internal interface for timeout callbacks
+type timeoutHelper interface {
+	resetElectionTimeout()
+}
+
 // Expose actual type for tests to use
-func newConsensusModuleImpl(persistentState PersistentState, log Log) *consensusModuleImpl {
+func newConsensusModuleImpl(
+	persistentState PersistentState,
+	log Log,
+	timeoutHelper timeoutHelper,
+) *consensusModuleImpl {
+	timeoutHelper.resetElectionTimeout()
 	return &consensusModuleImpl{
 		// #5.2-p1s2: When servers start up, they begin as followers
 		FOLLOWER,
@@ -39,4 +55,14 @@ func (cm *consensusModuleImpl) GetServerState() ServerState {
 func (cm *consensusModuleImpl) ProcessRpc(appendEntries AppendEntries) (AppendEntriesReply, error) {
 	success, err := cm._processRpc_AppendEntries(appendEntries)
 	return AppendEntriesReply{cm.persistentState.GetCurrentTerm(), success}, err
+}
+
+// #5.2-p1s5: If a follower receives no communication over a period of time
+// called the election timeout, then it assumes there is no viable leader
+// and begins an election to choose a new leader.
+// #5.2-p2s1: To begin an election, a follower increments its current term
+// and transitions to candidate state.
+func (cm *consensusModuleImpl) ElectionTimeout() {
+	cm.persistentState.SetCurrentTerm(cm.persistentState.GetCurrentTerm() + 1)
+	cm.serverState = CANDIDATE
 }
--- consensus_test.go
+++ consensus_test.go
@@ -6,12 +6,56 @@ import (
 
 // #5.2-p1s2: When servers start up, they begin as followers
 func TestCMStartsAsFollower(t *testing.T) {
-	cm := NewConsensusModule(newIMPSWithCurrentTerm(TEST_CURRENT_TERM), nil)
+	ps := newIMPSWithCurrentTerm(TEST_CURRENT_TERM)
+	th := new(mockTimeoutHelper)
+	cm := newConsensusModuleImpl(ps, nil, th)
 
 	if cm == nil {
 		t.Fatal()
 	}
 	if cm.GetServerState() != FOLLOWER {
+		t.Fatal()
+	}
+}
+
+//
+type mockTimeoutHelper struct {
+	resetElectionTimeoutCalled bool
+}
+
+func (mth *mockTimeoutHelper) resetElectionTimeout() {
+	mth.resetElectionTimeoutCalled = true
+}
+
+// #5.2-p1s5: If a follower receives no communication over a period of time
+// called the election timeout, then it assumes there is no viable leader
+// and begins an election to choose a new leader.
+// #5.2-p2s1: To begin an election, a follower increments its current term
+// and transitions to candidate state.
+func TestCMElectionTimeout(t *testing.T) {
+	ps := newIMPSWithCurrentTerm(TEST_CURRENT_TERM)
+	th := new(mockTimeoutHelper)
+	cm := newConsensusModuleImpl(ps, nil, th)
+
+	if cm == nil {
+		t.Fatal()
+	}
+	if cm.GetServerState() != FOLLOWER {
+		t.Fatal()
+	}
+
+	// Test that the election timeout callback is reset on creation
+	if !th.resetElectionTimeoutCalled {
+		t.Fatal()
+	}
+
+	// Test that election timeout causes a new election
+	th.resetElectionTimeoutCalled = false
+	cm.ElectionTimeout()
+	if ps.GetCurrentTerm() != TEST_CURRENT_TERM+1 {
+		t.Fatal()
+	}
+	if cm.GetServerState() != CANDIDATE {
 		t.Fatal()
 	}
 }
