c022485cd838196e45eb9ee335a26ebe4ec46a0c
Div Shekhar <div@txtlabs.com>
2015-09-05 16:41:01 -0700

    CandidateVolatileState and helpers functions

---
+++ state_candidate_test.go
@@ -0,0 +1,48 @@
+package raft
+
+import (
+	"testing"
+)
+
+func TestCandidateVolatileState(t *testing.T) {
+	cvs := newCandidateVolatileState(testPeerIds)
+
+	// Initial state
+	if cvs.receivedVotes != 1 {
+		t.Fatal()
+	}
+	if cvs.requiredVotes != 3 {
+		t.Fatal()
+	}
+
+	// Add a vote - no quorum yet
+	if cvs.addVoteFrom("s2") {
+		t.Fatal()
+	}
+
+	// Add 2nd vote - should be at quorum
+	if !cvs.addVoteFrom("s3") {
+		t.Fatal()
+	}
+
+	// Add remaining votes - should stay at quorum
+	if !cvs.addVoteFrom("s4") {
+		t.Fatal()
+	}
+	if !cvs.addVoteFrom("s5") {
+		t.Fatal()
+	}
+}
+
+func TestCandidateVolatileState_3nodes(t *testing.T) {
+	cvs := newCandidateVolatileState([]ServerId{"peer2", "peer3"})
+	if cvs.receivedVotes != 1 || cvs.requiredVotes != 2 {
+		t.Fatal()
+	}
+	if !cvs.addVoteFrom("peer3") {
+		t.Fatal()
+	}
+	if !cvs.addVoteFrom("peer2") {
+		t.Fatal()
+	}
+}
