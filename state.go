00b790ba8c46a048d72306c675f82b06950d26b9
Div Shekhar <div@txtlabs.com>
2015-10-23 23:19:56 -0700

    Make volatileState private

--- state.go
+++ state.go
@@ -1,7 +1,7 @@
 package raft
 
 // Volatile state on all servers
-type VolatileState struct {
+type volatileState struct {
 	// index of highest log entry known to be committed
 	// (initialized to 0, increases monotonically)
 	commitIndex LogIndex
