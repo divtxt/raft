31ce3c6b1d2bd27c3656209e9ff404eb5c4350f5
Div Shekhar <div@txtlabs.com>
2015-11-11 15:39:46 -0800

    Add serverState checks in _processRpc_... functions

--- consensus_requestvote.go
+++ consensus_requestvote.go
@@ -2,10 +2,25 @@
 
 package raft
 
+import (
+	"fmt"
+)
+
 func (cm *passiveConsensusModule) _processRpc_RequestVote(
 	fromPeer ServerId,
 	rpcRequestVote *RpcRequestVote,
 ) bool {
+	switch cm.serverState {
+	case FOLLOWER:
+		// Pass through to main logic below
+	case CANDIDATE:
+		panic("TODO: _processRpc_RequestVote / CANDIDATE")
+	case LEADER:
+		panic("TODO: _processRpc_RequestVote / LEADER")
+	default:
+		panic(fmt.Sprintf("FATAL: unknown ServerState: %v", cm.serverState))
+	}
+
 	// 1. Reply false if term < currentTerm (#5.1)
 	return false
 }
