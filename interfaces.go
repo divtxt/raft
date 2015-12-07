100b4475e48efad7db752ed8247cc13ffdde47e4
Div Shekhar <div@txtlabs.com>
2015-12-07 21:24:09 +0530

    Detailed comments for external interface implementors.

--- interfaces.go
+++ interfaces.go
@@ -1,4 +1,14 @@
 // Interfaces that users of this package must implement.
+//
+// Notes for implementers:
+//
+// - Concurrency: the consensus module will only ever call the methods from it's
+// single goroutine.
+//
+// - Errors: all errors should be indicated using panic(). This includes both
+// invalid parameters sent by the consensus module and internal errors in the
+// implementation. Note that such a panic will shutdown the consensus module.
+//
 
 package raft
 
@@ -24,14 +34,40 @@ type PersistentState interface {
 	SetCurrentTermAndVotedFor(currentTerm TermNo, votedFor ServerId)
 }
 
-// Asynchronous RPC sender.
-// See comments in rpctypes.go for an explanation of the messaging model.
+// Asynchronous RPC service.
+//
+// Since the consensus module is implemented as a single threaded component, an
+// asynchronous programming model is used for RPC.
+// In this model, RPC requests and replies are independent message types that
+// are sent and received using the same interfaces.
+//
+// The choice of RPC protocol is unspecified here.
+//
+// See rpctypes.go for the various RPC message types.
 type RpcSender interface {
 	// Send the given RPC message to the given server asynchronously.
-	// The ConsensusModule will only ever call this method from it's
-	// single goroutine.
-	// This call should always succeed but does not require any guarantee
-	// of delivery success.
+	//
+	// Notes for implementers:
+	//
+	// - No guarantee of success is expected.
+	//
+	// - When the RPC completes, the result should be sent via the consensus
+	// module's ProcessRpcAsync() function.
+	//
+	// - If the RPC fails, there is no need to notify the consensus module.
+	//
+	// - It is expected that multiple RPC messages will be sent independently to
+	// different servers.
+	//
+	// - The consensus module only expects to send one RPC to a given server.
+	// Since RPC failure is not reported to consensus module, implementations can
+	// choose how to handle extra RPCs to a server for which they already have an
+	// RPC in flight: cancel the first message, drop the second or enqueue it.
+	//
+	// - The RPC is time-sensitive and expected to be immediate. If any queueing
+	// or retrying is implemented, it should be very limited in time and queue
+	// size.
+	//
 	// TODO: should this call error if rpc type is unknown?
 	// TODO: should this call error if bad server id?
 	SendAsync(toServer ServerId, rpc interface{})
