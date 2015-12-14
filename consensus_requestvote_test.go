a22b83d754a105e7a2002519178e549528b2ab44
Div Shekhar <div@txtlabs.com>
2015-12-14 07:29:41 +0530

    Update RequestVote step 1 test data

--- consensus_requestvote_test.go
+++ consensus_requestvote_test.go
@@ -5,18 +5,14 @@ import (
 	"testing"
 )
 
-func makeRVWithTerm(term TermNo) *RpcRequestVote {
-	return &RpcRequestVote{term, 0, 0}
-}
-
 // 1. Reply false if term < currentTerm (#5.1)
-// Note: this test assumes server in sync with the Figure 7 leader
+// Note: test based on Figure 7; server is leader line; peer is case (a)
 func TestCM_RpcRV_TermLessThanCurrentTerm(t *testing.T) {
 	f := func(setup func(t *testing.T) (mcm *managedConsensusModule, mrs *mockRpcSender)) {
 		mcm, _ := setup(t)
 		serverTerm := mcm.pcm.persistentState.GetCurrentTerm()
 
-		requestVote := makeRVWithTerm(serverTerm - 1)
+		requestVote := &RpcRequestVote{7, 9, 6}
 
 		reply := mcm.pcm.rpc("s2", requestVote)
 
