9a9e7c075e7dbd8eb0609d63e609b0b92de6dfd1
Div Shekhar <div@txtlabs.com>
2015-12-16 16:34:01 +0530

    Handle newer term in AppendEntriesReply

--- consensus_appendentriesreply_test.go
+++ consensus_appendentriesreply_test.go
@@ -44,3 +44,27 @@ func TestCM_RpcAER_FollowerOrCandidate_PanicsForSameTermReply(t *testing.T) {
 	f(testSetupMCM_Follower_Figure7LeaderLine)
 	f(testSetupMCM_Candidate_Figure7LeaderLine)
 }
+
+// #RFS-A2: If RPC request or response contains term T > currentTerm:
+// set currentTerm = T, convert to follower (#5.1)
+// #5.1-p3s4: ...; if one server's current term is smaller than the
+// other's, then it updates its current term to the larger value.
+// #5.1-p3s5: If a candidate or leader discovers that its term is out of
+// date, it immediately reverts to follower state.
+func TestCM_RpcAER_Leader_NewerTerm(t *testing.T) {
+	mcm, _ := testSetupMCM_Leader_Figure7LeaderLine(t)
+	serverTerm := mcm.pcm.persistentState.GetCurrentTerm()
+	sentRpc := makeAEWithTerm(serverTerm)
+
+	mcm.pcm.rpcReply("s2", sentRpc, &RpcAppendEntriesReply{serverTerm + 1, false})
+	if mcm.pcm.getServerState() != FOLLOWER {
+		t.Fatal()
+	}
+	if mcm.pcm.persistentState.GetCurrentTerm() != serverTerm+1 {
+		t.Fatal()
+	}
+	if mcm.pcm.persistentState.GetVotedFor() != "" {
+		t.Fatal()
+	}
+
+}
