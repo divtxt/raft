037cfc4ceefd68889c6a45bed46b04901e98eae9
Div Shekhar <div@txtlabs.com>
2015-12-16 16:34:01 +0530

    Handle newer term in AppendEntriesReply

--- consensus_appendentriesreply.go
+++ consensus_appendentriesreply.go
@@ -33,6 +33,20 @@ func (cm *passiveConsensusModule) _processRpc_AppendEntriesReply(
 		))
 	case LEADER:
 		// Pass through to main logic below
-		panic("TODO: _processRpc_AppendEntriesReply / LEADER")
 	}
+
+	// #RFS-A2: If RPC request or response contains term T > currentTerm:
+	// set currentTerm = T, convert to follower (#5.1)
+	// #5.1-p3s4: ...; if one server's current term is smaller than the
+	// other's, then it updates its current term to the larger value.
+	// #5.1-p3s5: If a candidate or leader discovers that its term is out of
+	// date, it immediately reverts to follower state.
+	senderCurrentTerm := appendEntriesReply.Term
+	if senderCurrentTerm > serverTerm {
+		cm.becomeFollowerWithTerm(senderCurrentTerm)
+		// TODO: test for this
+		return
+	}
+
+	panic("TODO: _processRpc_AppendEntriesReply / LEADER")
 }
