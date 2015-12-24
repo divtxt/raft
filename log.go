818b1f3bd0cb8a3149d37062af25ce02cabc6ea2
Div Shekhar <div@txtlabs.com>
2015-12-24 12:30:34 -0800

    Move helper fuction to separate file

--- log.go
+++ log.go
@@ -95,13 +95,3 @@ type Log interface {
 	// if lastApplied == indexOfLastEntry.
 	ApplyNextCommandToStateMachine()
 }
-
-// Helper method
-func GetIndexAndTermOfLastEntry(log Log) (LogIndex, TermNo) {
-	lastLogIndex := log.GetIndexOfLastEntry()
-	var lastLogTerm TermNo = 0
-	if lastLogIndex > 0 {
-		lastLogTerm = log.GetTermAtIndex(lastLogIndex)
-	}
-	return lastLogIndex, lastLogTerm
-}
