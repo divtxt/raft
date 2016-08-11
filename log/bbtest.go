bda8030abe61aa734520a95041680d7bd4cf9f8e
Div Shekhar <div@txtlabs.com>
2016-08-10 22:22:22 -0700

    Separate Log and StateMachine interfaces; InMemoryLog implementation

---
+++ log/bbtest.go
@@ -0,0 +1,140 @@
+package log
+
+import (
+	"bytes"
+	. "github.com/divtxt/raft"
+	"reflect"
+	"testing"
+)
+
+// Log with 10 entries with terms as shown in Figure 7, leader line
+func BlackboxTest_MakeFigure7LeaderLineTerms() []TermNo {
+	return []TermNo{1, 1, 1, 4, 4, 5, 5, 6, 6, 6}
+}
+
+// Helper
+func TestCommandEquals(c Command, s string) bool {
+	return bytes.Equal(c, Command(s))
+}
+
+// Blackbox test.
+// Send a Log with 10 entries with terms as shown in Figure 7, leader line.
+// Entries should be Command("c1"), Command("c2"), etc.
+func BlackboxTest_Log(t *testing.T, log Log) {
+	// Initial data tests
+	iole, err := log.GetIndexOfLastEntry()
+	if err != nil {
+		t.Fatal()
+	}
+	if iole != 10 {
+		t.Fatal()
+	}
+	term, err := log.GetTermAtIndex(10)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if term != 6 {
+		t.Fatal()
+	}
+
+	// get entries test
+	le := TestHelper_GetLogEntryAtIndex(log, 10)
+	if le.TermNo != 6 {
+		t.Fatal(le.TermNo)
+	}
+	if !TestCommandEquals(le.Command, "c10") {
+		t.Fatal(le.Command)
+	}
+
+	var logEntries []LogEntry
+
+	// set test - invalid index
+	logEntries = []LogEntry{{8, Command("c12")}}
+	err = log.SetEntriesAfterIndex(11, logEntries)
+	if err == nil {
+		t.Fatal()
+	}
+
+	// set test - no replacing
+	logEntries = []LogEntry{{7, Command("c11")}, {8, Command("c12")}}
+	err = log.SetEntriesAfterIndex(10, logEntries)
+	if err != nil {
+		t.Fatal()
+	}
+	iole, err = log.GetIndexOfLastEntry()
+	if err != nil {
+		t.Fatal()
+	}
+	if iole != 12 {
+		t.Fatal()
+	}
+	le = TestHelper_GetLogEntryAtIndex(log, 12)
+	if !reflect.DeepEqual(le, LogEntry{8, Command("c12")}) {
+		t.Fatal(le)
+	}
+
+	// set test - partial replacing
+	logEntries = []LogEntry{{7, Command("c11")}, {9, Command("c12")}, {9, Command("c13'")}}
+	err = log.SetEntriesAfterIndex(10, logEntries)
+	if err != nil {
+		t.Fatal()
+	}
+	iole, err = log.GetIndexOfLastEntry()
+	if err != nil {
+		t.Fatal()
+	}
+	if iole != 13 {
+		t.Fatal()
+	}
+	le = TestHelper_GetLogEntryAtIndex(log, 12)
+	if !reflect.DeepEqual(le, LogEntry{9, Command("c12")}) {
+		t.Fatal(le)
+	}
+
+	// append test
+	err = log.AppendEntry(8, Command("c14"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	iole, err = log.GetIndexOfLastEntry()
+	if err != nil {
+		t.Fatal()
+	}
+	if iole != 14 {
+		t.Fatal()
+	}
+	le = TestHelper_GetLogEntryAtIndex(log, 14)
+	if !reflect.DeepEqual(le, LogEntry{8, Command("c14")}) {
+		t.Fatal(le)
+	}
+
+	// set test - no new entries with empty slice
+	logEntries = []LogEntry{}
+	err = log.SetEntriesAfterIndex(3, logEntries)
+	if err != nil {
+		t.Fatal()
+	}
+	iole, err = log.GetIndexOfLastEntry()
+	if err != nil {
+		t.Fatal()
+	}
+	if iole != 3 {
+		t.Fatal()
+	}
+	le = TestHelper_GetLogEntryAtIndex(log, 3)
+	if !reflect.DeepEqual(le, LogEntry{1, Command("c3")}) {
+		t.Fatal(le)
+	}
+}
+
+// Helper
+func TestHelper_GetLogEntryAtIndex(log Log, li LogIndex) LogEntry {
+	if li == 0 {
+		panic("oops!")
+	}
+	entries, err := log.GetEntriesAfterIndex(li - 1)
+	if err != nil {
+		panic(err)
+	}
+	return entries[0]
+}
---
+++ log/inmemlog.go
@@ -0,0 +1,102 @@
+package log
+
+import (
+	"errors"
+	"fmt"
+	. "github.com/divtxt/raft"
+)
+
+// In-memory implementation of locking as raft.Log
+
+type InMemoryLog struct {
+	entries                  []LogEntry
+	MaxEntriesPerAppendEntry uint64
+}
+
+func NewInMemoryLog(maxEntriesPerAppendEntry uint64) *InMemoryLog {
+	// FIXME: move maxEntriesPerAppendEntry out of here?!
+	if maxEntriesPerAppendEntry <= 0 {
+		panic("maxEntriesPerAppendEntry must be greater than zero")
+	}
+	entries := []LogEntry{}
+	iml := &InMemoryLog{entries, maxEntriesPerAppendEntry}
+	return iml
+}
+
+func (iml *InMemoryLog) GetIndexOfLastEntry() (LogIndex, error) {
+	return LogIndex(len(iml.entries)), nil
+}
+
+func (iml *InMemoryLog) GetTermAtIndex(li LogIndex) (TermNo, error) {
+	if li == 0 {
+		return 0, errors.New("GetTermAtIndex(): li=0")
+	}
+	if li > LogIndex(len(iml.entries)) {
+		return 0, fmt.Errorf(
+			"GetTermAtIndex(): li=%v > iole=%v", li, len(iml.entries),
+		)
+	}
+	return iml.entries[li-1].TermNo, nil
+}
+
+func (iml *InMemoryLog) GetEntriesAfterIndex(afterLogIndex LogIndex) ([]LogEntry, error) {
+	iole, err := iml.GetIndexOfLastEntry()
+	if err != nil {
+		return nil, err
+	}
+
+	if iole < afterLogIndex {
+		return nil, fmt.Errorf(
+			"afterLogIndex=%v is > iole=%v",
+			afterLogIndex,
+			iole,
+		)
+	}
+
+	var numEntriesToGet uint64 = uint64(iole - afterLogIndex)
+
+	// Short-circuit allocation for common case
+	if numEntriesToGet == 0 {
+		return []LogEntry{}, nil
+	}
+
+	if numEntriesToGet > iml.MaxEntriesPerAppendEntry {
+		numEntriesToGet = iml.MaxEntriesPerAppendEntry
+	}
+
+	logEntries := make([]LogEntry, numEntriesToGet)
+	var i uint64 = 0
+	nextIndexToGet := afterLogIndex + 1
+
+	for i < numEntriesToGet {
+		logEntries[i] = iml.entries[nextIndexToGet-1]
+		i++
+		nextIndexToGet++
+	}
+
+	return logEntries, nil
+}
+
+func (iml *InMemoryLog) SetEntriesAfterIndex(li LogIndex, entries []LogEntry) error {
+	iole, err := iml.GetIndexOfLastEntry()
+	if err != nil {
+		return err
+	}
+	if iole < li {
+		return fmt.Errorf("InMemoryLog: setEntriesAfterIndex(%d, ...) but iole=%d", li, iole)
+	}
+	// delete entries after index
+	if iole > li {
+		iml.entries = iml.entries[:li]
+	}
+	// append entries
+	iml.entries = append(iml.entries, entries...)
+	return nil
+}
+
+func (iml *InMemoryLog) AppendEntry(termNo TermNo, command Command) error {
+	// FIXME: take one LogEntry param
+	entry := LogEntry{termNo, command}
+	iml.entries = append(iml.entries, entry)
+	return nil
+}
