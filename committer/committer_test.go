d639902396674d08923ad054ee4820cbe6ffeab3
Div Shekhar <div@txtlabs.com>
2017-01-15 22:49:47 +0530

    Refactor DummyStateMachine

--- committer/committer_test.go
+++ committer/committer_test.go
@@ -1,12 +1,8 @@
 package committer
 
 import (
-	"bytes"
-	"fmt"
-	"strconv"
 	"testing"
 
-	. "github.com/divtxt/raft"
 	"github.com/divtxt/raft/log"
 	"github.com/divtxt/raft/testhelpers"
 )
@@ -15,7 +11,7 @@ import (
 // log[lastApplied] to state machine (#5.3)
 func TestCommitter(t *testing.T) {
 	iml := log.TestUtil_NewInMemoryLog_WithFigure7LeaderLine()
-	dsm := NewDummyStateMachine(3)
+	dsm := testhelpers.NewDummyStateMachine(3)
 
 	committer := NewCommitter(iml, dsm)
 
@@ -25,6 +21,9 @@ func TestCommitter(t *testing.T) {
 	// Test StopSync() - this also waits for the first run to complete.
 	committer.StopSync()
 	if dsm.GetLastApplied() != 4 {
+		t.Fatal()
+	}
+	if !dsm.AppliedCommandsEqual(4) {
 		t.Fatal()
 	}
 
@@ -39,6 +38,9 @@ func TestCommitter(t *testing.T) {
 	if dsm.GetLastApplied() != 8 {
 		t.Fatal()
 	}
+	if !dsm.AppliedCommandsEqual(4, 5, 6, 7, 8) {
+		t.Fatal()
+	}
 
 	// Regressing commitIndex should panic
 	testhelpers.TestHelper_ExpectPanic(
@@ -46,49 +48,4 @@ func TestCommitter(t *testing.T) {
 		func() { committer.CommitIndexChanged(7) },
 		"FATAL: decreasing commitIndex: 8 > 7",
 	)
-}
-
-// -- MOVE TO DSM
-
-// Dummy state machine that implements StateMachine.
-// Does not provide any useful state or commands. Meant only for tests.
-type DummyStateMachine struct {
-	lastApplied LogIndex
-}
-
-// Will serialize to Command("cN")
-func DummyCommand(N int) Command {
-	return Command("c" + strconv.Itoa(N))
-}
-
-func NewDummyStateMachine(lastApplied LogIndex) *DummyStateMachine {
-	return &DummyStateMachine{lastApplied}
-}
-
-func (dsm *DummyStateMachine) GetLastApplied() LogIndex {
-	return dsm.lastApplied
-}
-
-func (dsm *DummyStateMachine) ApplyCommand(logIndex LogIndex, command Command) {
-	if logIndex < dsm.lastApplied {
-		panic(fmt.Sprintf(
-			"DummyStateMachine: logIndex=%d is < current lastApplied=%d",
-			logIndex,
-			dsm.lastApplied,
-		))
-	}
-	if !DummyCommandEquals(command, int(logIndex)) {
-		panic(fmt.Sprintf(
-			"DummyStateMachine: command=%v is wrong for logIndex=%v",
-			command,
-			logIndex,
-		))
-	}
-	dsm.lastApplied = logIndex
-}
-
-// Helper
-func DummyCommandEquals(c Command, n int) bool {
-	cn := Command("c" + strconv.Itoa(n))
-	return bytes.Equal(c, cn)
 }
