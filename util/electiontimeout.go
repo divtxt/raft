8f5af8cd9de9d7943b8b878f2779cd13a2ec86fd
Div Shekhar <div@getclockwise.com>
2020-01-23 05:04:56 +0530

    ElectionTimeoutChooser shares Rand

--- util/electiontimeout.go
+++ util/electiontimeout.go
@@ -5,20 +5,20 @@ import (
 	"time"
 )
 
+var r *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
 type ElectionTimeoutChooser struct {
-	r                  *rand.Rand
 	electionTimeoutLow time.Duration
 }
 
 func NewElectionTimeoutChooser(electionTimeoutLow time.Duration) *ElectionTimeoutChooser {
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	return &ElectionTimeoutChooser{r, electionTimeoutLow}
+	return &ElectionTimeoutChooser{electionTimeoutLow}
 }
 
-func (ret *ElectionTimeoutChooser) ChooseRandomElectionTimeout() time.Duration {
+func (etc *ElectionTimeoutChooser) ChooseRandomElectionTimeout() time.Duration {
 	// #5.2-p6s2: ..., election timeouts are chosen randomly from a fixed
 	// interval (e.g., 150-300ms)
 	// Currently, we choose a time between electionTimeoutLow and 2*electionTimeoutLow
-	timeout := ret.electionTimeoutLow + time.Duration(ret.r.Int63n(int64(ret.electionTimeoutLow)+1))
+	timeout := etc.electionTimeoutLow + time.Duration(r.Int63n(int64(etc.electionTimeoutLow)+1))
 	return timeout
 }
